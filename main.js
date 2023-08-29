7b0c1fd6be271b4800475bd076e0e33ea45a19bd
CoDinAIy <139142598+CoDinAIy@users.noreply.github.com>
2023-08-29 15:15:08 +0100

    added max tasks, delete icon and event listener to remove task when clicked

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 const input = document.querySelector(".listInput")
 input.addEventListener("keypress", getInput)
 const toDoItems = document.querySelector(".toDoItems")
+let noOfLists = 0
 
 function getInput(e) {
     if (e.key === "Enter") {
-        const final = input.value
-        console.log(final)
-        makeNewList(final)
+        if (noOfLists != 10) {
+            const final = input.value
+            console.log(final)
+            makeNewList(final)
+            noOfLists += 1
+        } else {
+            alert("Max 10 tasks!")
+        }
     }
 }
 
 function makeNewList(final) {
+    const task = document.createElement("div")
+    task.classList.add("task")
+
     const content = document.createElement("li")
     content.classList.add("content")
     content.textContent = final
-    toDoItems.appendChild(content)
+
+    const deleteTask = document.createElement("img")
+    deleteTask.classList.add("binIcon")
+    deleteTask.src = "images/binIcon.jpg"
+    deleteTask.addEventListener("click", function deleteTheTask(){
+        task.removeChild(content)
+        task.removeChild(deleteTask)
+        noOfLists -= 1
+    })
+
+    task.appendChild(content)
+    task.appendChild(deleteTask)
+    toDoItems.appendChild(task)
+
 }
