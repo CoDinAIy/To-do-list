55005fd9deb7ce4a4f936331ab24a23fe429cd0b
CoDinAIy <139142598+CoDinAIy@users.noreply.github.com>
2023-08-31 12:15:34 +0100

    added sidebar and footer features, list interaction works without bugs

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ let noOfLists = 0
 function getInput(e) {
     if (e.key === "Enter") {
         if (noOfLists != 10) {
+            noOfLists += 1
             const final = input.value
             console.log(final)
-            makeNewList(final)
-            noOfLists += 1
+            makeNewList(final)  
+            input.value = ""
+            
         } else {
             alert("Max 10 tasks!")
         }
@@ -17,24 +19,25 @@ function getInput(e) {
 }
 
 function makeNewList(final) {
-    const task = document.createElement("div")
+    const task = document.createElement("li")
     task.classList.add("task")
-
-    const content = document.createElement("li")
-    content.classList.add("content")
-    content.textContent = final
+    
+    const taskContent = document.createElement("li")
+    taskContent.textContent = final
 
     const deleteTask = document.createElement("img")
     deleteTask.classList.add("binIcon")
     deleteTask.src = "images/binIcon.jpg"
-    deleteTask.addEventListener("click", function deleteTheTask(){
-        task.removeChild(content)
-        task.removeChild(deleteTask)
+    deleteTask.addEventListener("click", function deleteTask() {
+        toDoItems.removeChild(task)
         noOfLists -= 1
-    })
+    } )
 
-    task.appendChild(content)
+    task.appendChild(taskContent)
     task.appendChild(deleteTask)
     toDoItems.appendChild(task)
 
 }
+
+
+
